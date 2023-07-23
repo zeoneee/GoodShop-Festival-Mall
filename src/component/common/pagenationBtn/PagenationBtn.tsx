64976dc04fd97b5bb8ad84d549e8b75db445ef85
tnfus916 <tnfus1999@gmail.com>
2023-07-24 02:12:08 +0900

    메인색 변경
    버튼, 헤더(로고 제외), 상품 pagenation 버튼 노란색으로 변경
    회원가입시 이용약관동의 문구에서 링크 제거

--- src/component/common/pagenationBtn/PagenationBtn.tsx
+++ src/component/common/pagenationBtn/PagenationBtn.tsx
@@ -1,16 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import * as S from "./PagenationBtnStyle";
-import { SVGIcon } from '../../icon/SVGIcon';
+import { SVGIcon } from "../../icon/SVGIcon";
 
-
-interface PagenationBtnProps{
+interface PagenationBtnProps {
   pageNumber: number;
   nowPage?: number;
   setPage: (currentPage: number) => void;
 }
 
-export default function PagenationBtn({pageNumber, setPage} : PagenationBtnProps ) {
-
+export default function PagenationBtn({
+  pageNumber,
+  setPage,
+}: PagenationBtnProps) {
   const [nowPage, setNowPage] = useState(1);
 
   const handlePageBtn = (index: number) => {
@@ -21,25 +22,32 @@ export default function PagenationBtn({pageNumber, setPage} : PagenationBtnProps
   const onLeftArrow = nowPage !== 1;
   const onRightArrow = pageNumber !== nowPage;
 
-
   return (
     <S.PagenationBtnWrapper>
-    <S.PrevBtn onClick={() => handlePageBtn(nowPage - 1)} disabled={!onLeftArrow}>
-      Prev
-    </S.PrevBtn>
-    {Array.from({ length: pageNumber }).map((_, index) => (
-      <S.PageNumberBtn
-        key={index} 
-        onClick={() => {
-        setPage(index+1); 
-        setNowPage(index+1);
-      }} 
-      on={nowPage === Number(index + 1) ? "on" : "off"}
+      <S.PrevBtn
+        onClick={() => handlePageBtn(nowPage - 1)}
+        disabled={!onLeftArrow}
       >
-        {index+1}
-      </S.PageNumberBtn>
-    ))}
-    <S.NextBtn onClick={() => handlePageBtn(nowPage + 1)} disabled={!onRightArrow}>Next</S.NextBtn>
+        Prev
+      </S.PrevBtn>
+      {Array.from({ length: pageNumber }).map((_, index) => (
+        <S.PageNumberBtn
+          key={index}
+          onClick={() => {
+            setPage(index + 1);
+            setNowPage(index + 1);
+          }}
+          on={nowPage === Number(index + 1) ? "on" : "off"}
+        >
+          {index + 1}
+        </S.PageNumberBtn>
+      ))}
+      <S.NextBtn
+        onClick={() => handlePageBtn(nowPage + 1)}
+        disabled={!onRightArrow}
+      >
+        Next
+      </S.NextBtn>
     </S.PagenationBtnWrapper>
-  )
+  );
 }
