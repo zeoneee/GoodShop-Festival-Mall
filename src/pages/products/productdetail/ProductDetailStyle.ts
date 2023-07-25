3cd4b7ca2564fd77af6d9f32c18ce7dbd580237b
tnfus916 <tnfus1999@gmail.com>
2023-07-25 21:37:30 +0900

    제품 상세 페이지 총 수량 출력
    - , + 버튼 클릭했을 때 총 버튼 내 수량과 하단 총 수량 출력 모두 변경 되도록 수정

--- src/pages/products/productdetail/ProductDetailStyle.ts
+++ src/pages/products/productdetail/ProductDetailStyle.ts
@@ -1,102 +1,110 @@
 import styled from "styled-components";
 
 const ProductWrapper = styled.section`
-    display: grid;
+  display: grid;
   grid-template-columns: 1fr 1fr;
-    gap: 50px;
-    margin: 80px;
-`
+  gap: 50px;
+  margin: 80px;
+`;
 const ImgWrapper = styled.div`
+  width: 100%;
+  max-height: 600px;
+  img {
+    display: block;
     width: 100%;
-    max-height: 600px;
-    img {
-      display: block;
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-`
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
 const DetailWrapper = styled.div`
-    width: 100%;
-`
+  width: 100%;
+`;
 
 const InfoWrapper = styled.div`
-    margin-bottom: 138px;
-`
+  margin-bottom: 138px;
+`;
 
-const ProductImg = styled.img`
-
-`
+const ProductImg = styled.img``;
 const ProductStoreName = styled.a`
-    font-size: 18px;
-    line-height: 23px;
-    color: var(--color-darkGrey);
-    &:hover {
-      text-decoration: underline;
-      cursor: pointer;
-    }
-`
+  font-size: 18px;
+  line-height: 23px;
+  color: var(--color-darkGrey);
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
 
 const ProductName = styled.strong`
-    display: block;
-    margin: 16px auto 20px;
-    font-size: 36px;
-    line-height: 45px;
-`
+  display: block;
+  margin: 16px auto 20px;
+  font-size: 36px;
+  line-height: 45px;
+`;
 
 const ProductPrice = styled.p`
-    font-size: 36px;
-    line-height: 45px;
-    font-weight: 700;
-    &::after{
-        content: "원";
-        font-size: 18px;
-        line-height: 23px;
-        margin-left: 5px;
-    }
-  
-`
+  font-size: 36px;
+  line-height: 45px;
+  font-weight: 700;
+  &::after {
+    content: "원";
+    font-size: 18px;
+    line-height: 23px;
+    margin-left: 5px;
+  }
+`;
 
 const ShipInfoWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-bottom: 20px;
-    border-bottom: 3px solid var(--color-grey);
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom: 3px solid var(--color-grey);
+`;
 
 const Shipping = styled.p`
-    font-size: 16px;
-    color: var(--color-darkGrey);
-`
+  font-size: 16px;
+  color: var(--color-darkGrey);
+`;
 
 const ProductStock = styled.p`
-    font-size: 16px;
-    color: var(--color-red);
-`
+  font-size: 16px;
+  color: var(--color-red);
+`;
 
-const TotalPriceTxt = styled.p`
-
-`
+const TotalPriceTxt = styled.p``;
 
 const ButtonWrapper = styled.div`
-    display: flex;
-    gap: 14px;
-`
+  display: flex;
+  gap: 14px;
+`;
 
 const PriceWrapper = styled.div`
+  width: 100%;
+`;
 
-`
+const TotalPriceWrapper = styled.span``;
 
-const TotalPriceWrapper = styled.div`
-`
+const ResultAmountTxt = styled.p``;
 
-const ResultAmountTxt = styled.p`
-
-`
-
-const TotalPrice = styled.p`
-
-`
-export {ProductStoreName, ProductName, ProductPrice, ProductImg, Shipping, ProductStock, TotalPriceTxt, ImgWrapper, ProductWrapper, DetailWrapper, InfoWrapper, ShipInfoWrapper, ButtonWrapper, PriceWrapper, TotalPriceWrapper, ResultAmountTxt, TotalPrice}
+const TotalPrice = styled.p``;
+export {
+  ProductStoreName,
+  ProductName,
+  ProductPrice,
+  ProductImg,
+  Shipping,
+  ProductStock,
+  TotalPriceTxt,
+  ImgWrapper,
+  ProductWrapper,
+  DetailWrapper,
+  InfoWrapper,
+  ShipInfoWrapper,
+  ButtonWrapper,
+  PriceWrapper,
+  TotalPriceWrapper,
+  ResultAmountTxt,
+  TotalPrice,
+};
