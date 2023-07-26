7335972a3669108d7e74cacf8ab15186a2de35d8
tnfus916 <tnfus1999@gmail.com>
2023-07-26 13:17:19 +0900

    Design: Product Detail Page 상품 상세 정보 재배치상품 상세 정보 재배치

--- src/pages/products/productdetail/ProductDetail.tsx
+++ src/pages/products/productdetail/ProductDetail.tsx
@@ -57,11 +57,11 @@ export default function ProductDetail() {
 
             {/* 총 가격 부분 */}
             <S.PriceWrapper>
-              <S.TotalPriceTxt>총 상품 금액</S.TotalPriceTxt>
+              <S.ResultAmountTxt>
+                총 수량 <span>{selectedCount}</span>개
+              </S.ResultAmountTxt>
               <S.TotalPriceWrapper>
-                <S.ResultAmountTxt>
-                  총 수량 <span>{selectedCount}</span>개
-                </S.ResultAmountTxt>
+                <S.TotalPriceTxt>총 상품 금액</S.TotalPriceTxt>
                 <S.TotalPrice>
                   {selectedCount > 0
                     ? (state.price * selectedCount).toLocaleString()
@@ -81,7 +81,7 @@ export default function ProductDetail() {
           </S.DetailWrapper>
           <DetailTab />
         </S.ProductWrapper>
-      }
+      )}
     </>
   );
 }
